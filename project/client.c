859f097cadf389316580b7cac24ccf7f81bf280c
nicholas-callen <nicholascallen13@gmail.com>
2025-06-13 21:36:09 -0700

    Implement client and server for test_client_hello and test_server_hello

--- project/client.c
+++ project/client.c
@@ -1,35 +1,87 @@
-#include "consts.h"
-#include "io.h"
-#include "transport.h"
+#include "libsecurity/libsecurity.h"
+#include "tlv.h"
+
 #include <arpa/inet.h>
+#include <netdb.h>
+#include <openssl/rand.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-int main(int argc, char** argv) {
-    if (argc < 3) {
-        fprintf(stderr, "Usage: client <hostname> <port> \n");
-        exit(1);
+#define NONCE_TYPE 0x01
+#define PUBLIC_KEY_TYPE 0x02
+#define CLIENT_HELLO_TYPE 0x10
+
+void die(const char* msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+void send_all(int sockfd, const uint8_t* buf, size_t len) {
+    size_t total_sent = 0;
+    while (total_sent < len) {
+        ssize_t sent = send(sockfd, buf + total_sent, len - total_sent, 0);
+        if (sent <= 0) die("send");
+        total_sent += sent;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: ./client <hostname> <port>\n");
+        exit(EXIT_FAILURE);
     }
 
-    /* Create sockets */
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    // use IPv4  use UDP
+    const char* hostname = argv[1];
+    const char* port = argv[2];
 
-    /* Construct server address */
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET; // use IPv4
-    // Only supports localhost as a hostname, but that's all we'll test on
-    char* addr = strcmp(argv[1], "localhost") == 0 ? "127.0.0.1" : argv[1];
-    server_addr.sin_addr.s_addr = inet_addr(addr);
-    // Set sending port
-    int PORT = atoi(argv[2]);
-    server_addr.sin_port = htons(PORT); // Big endian
-  
-    init_io();
-    listen_loop(sockfd, &server_addr, CLIENT_START, input_io, output_io);
+    // === Create TCP socket and connect ===
+    struct addrinfo hints = {0}, *res;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if (getaddrinfo(hostname, port, &hints, &res) != 0) die("getaddrinfo");
+
+    int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (sockfd < 0) die("socket");
+
+    if (connect(sockfd, res->ai_addr, res->ai_addrlen) < 0) die("connect");
+    freeaddrinfo(res);
+
+    // === Build Client Hello ===
+    uint8_t nonce[32];
+    if (!RAND_bytes(nonce, sizeof(nonce))) die("RAND_bytes");
+
+    Key* my_key = generate_key();
+    size_t pubkey_len;
+    uint8_t* pubkey = get_public_key_bytes(my_key, &pubkey_len);
+
+    uint8_t *nonce_tlv, *pubkey_tlv, *client_hello;
+    size_t nonce_tlv_len, pubkey_tlv_len, client_hello_len;
+
+    tlv_create(NONCE_TYPE, nonce, sizeof(nonce), &nonce_tlv, &nonce_tlv_len);
+    tlv_create(PUBLIC_KEY_TYPE, pubkey, pubkey_len, &pubkey_tlv, &pubkey_tlv_len);
+
+    size_t inner_len = nonce_tlv_len + pubkey_tlv_len;
+    uint8_t* inner = malloc(inner_len);
+    memcpy(inner, nonce_tlv, nonce_tlv_len);
+    memcpy(inner + nonce_tlv_len, pubkey_tlv, pubkey_tlv_len);
+
+    tlv_create(CLIENT_HELLO_TYPE, inner, inner_len, &client_hello, &client_hello_len);
+
+    // === Send Client Hello ===
+    send_all(sockfd, client_hello, client_hello_len);
+    printf("Client Hello sent (%lu bytes)\n", client_hello_len);
+
+    // Clean up
+    free(nonce_tlv);
+    free(pubkey_tlv);
+    free(inner);
+    free(client_hello);
+    free(pubkey);
+    close(sockfd);
 
     return 0;
 }
--- project/server.c
+++ project/server.c
@@ -1,46 +1,139 @@
-#include "consts.h"
-#include "transport.h"
-#include "io.h"
+#include "libsecurity/libsecurity.h"
+#include "tlv.h"
+
 #include <arpa/inet.h>
+#include <openssl/rand.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: server <port>\n");
-        exit(1);
+#define NONCE_TYPE 0x01
+#define PUBLIC_KEY_TYPE 0x02
+#define CLIENT_HELLO_TYPE 0x10
+
+#define DNS_NAME_TYPE 0xA1
+#define SIGNATURE_TYPE 0xA2
+#define CERTIFICATE_TYPE 0xA0
+#define HANDSHAKE_SIGNATURE_TYPE 0x21
+#define SERVER_HELLO_TYPE 0x20
+
+#define MAX_BUF 4096
+
+void die(const char* msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+void send_all(int sockfd, const uint8_t* buf, size_t len) {
+    size_t sent = 0;
+    while (sent < len) {
+        ssize_t r = send(sockfd, buf + sent, len - sent, 0);
+        if (r <= 0) die("send");
+        sent += r;
+    }
+}
+
+ssize_t recv_all(int sockfd, uint8_t* buf, size_t maxlen) {
+    ssize_t len = recv(sockfd, buf, maxlen, 0);
+    if (len < 0) die("recv");
+    return len;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: ./server <port>\n");
+        exit(EXIT_FAILURE);
     }
 
-    /* Create sockets */
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    // use IPv4  use UDP
+    int port = atoi(argv[1]);
 
-    /* Construct our address */
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET; // use IPv4
-    server_addr.sin_addr.s_addr =
-        INADDR_ANY; // accept all connections
-                    // same as inet_addr("0.0.0.0")
-                    // "Address string to network bytes"
-    // Set receiving port
-    int PORT = atoi(argv[1]);
-    server_addr.sin_port = htons(PORT); // Big endian
+    // === Setup TCP socket ===
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) die("socket");
 
-    /* Let operating system know about our config */
-    bind(sockfd, (struct sockaddr*) &server_addr, sizeof(server_addr));
+    struct sockaddr_in addr = {0};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(port);
 
-    struct sockaddr_in client_addr; // Same information, but about client
-    socklen_t s = sizeof(struct sockaddr_in);
-    char buffer;
+    if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) < 0) die("bind");
+    if (listen(sockfd, 1) < 0) die("listen");
 
-    // Wait for client connection
-    recvfrom(sockfd, &buffer, sizeof(buffer), MSG_PEEK,
-                                   (struct sockaddr*) &client_addr, &s);
+    int clientfd = accept(sockfd, NULL, NULL);
+    if (clientfd < 0) die("accept");
 
-    init_io();
-    listen_loop(sockfd, &client_addr, SERVER_AWAIT, input_io, output_io);
+    // === Receive Client Hello ===
+    uint8_t buf[MAX_BUF];
+    ssize_t client_hello_len = recv_all(clientfd, buf, MAX_BUF);
+    if (buf[0] != CLIENT_HELLO_TYPE) exit(6);  // Unexpected message
+
+    uint8_t* client_hello = malloc(client_hello_len);
+    memcpy(client_hello, buf, client_hello_len);
+
+    // === Build Server Hello ===
+    uint8_t nonce[32];
+    RAND_bytes(nonce, sizeof(nonce));
+
+    Key* priv_key = read_private_key("server_key.bin");
+    size_t cert_len;
+    uint8_t* cert = read_bytes_from_file("server_cert.bin", &cert_len);
+
+    Key* ephemeral_key = generate_key();
+    size_t eph_pubkey_len;
+    uint8_t* eph_pubkey = get_public_key_bytes(ephemeral_key, &eph_pubkey_len);
+
+    // Signature input: client_hello || nonce || cert || ephemeral_pubkey
+    size_t sig_input_len = client_hello_len + sizeof(nonce) + cert_len + eph_pubkey_len;
+    uint8_t* sig_input = malloc(sig_input_len);
+    uint8_t* ptr = sig_input;
+    memcpy(ptr, client_hello, client_hello_len); ptr += client_hello_len;
+    memcpy(ptr, nonce, sizeof(nonce)); ptr += sizeof(nonce);
+    memcpy(ptr, cert, cert_len); ptr += cert_len;
+    memcpy(ptr, eph_pubkey, eph_pubkey_len);
+
+    size_t sig_len;
+    uint8_t* signature = sign(priv_key, sig_input, sig_input_len, &sig_len);
+
+    // TLV Encoding
+    uint8_t *nonce_tlv, *cert_tlv, *pubkey_tlv, *sig_tlv, *server_hello;
+    size_t nonce_tlv_len, cert_tlv_len, pubkey_tlv_len, sig_tlv_len, server_hello_len;
+
+    tlv_create(NONCE_TYPE, nonce, sizeof(nonce), &nonce_tlv, &nonce_tlv_len);
+    tlv_create(CERTIFICATE_TYPE, cert, cert_len, &cert_tlv, &cert_tlv_len);
+    tlv_create(PUBLIC_KEY_TYPE, eph_pubkey, eph_pubkey_len, &pubkey_tlv, &pubkey_tlv_len);
+    tlv_create(HANDSHAKE_SIGNATURE_TYPE, signature, sig_len, &sig_tlv, &sig_tlv_len);
+
+    size_t inner_len = nonce_tlv_len + cert_tlv_len + pubkey_tlv_len + sig_tlv_len;
+    uint8_t* inner = malloc(inner_len);
+    ptr = inner;
+    memcpy(ptr, nonce_tlv, nonce_tlv_len); ptr += nonce_tlv_len;
+    memcpy(ptr, cert_tlv, cert_tlv_len); ptr += cert_tlv_len;
+    memcpy(ptr, pubkey_tlv, pubkey_tlv_len); ptr += pubkey_tlv_len;
+    memcpy(ptr, sig_tlv, sig_tlv_len);
+
+    tlv_create(SERVER_HELLO_TYPE, inner, inner_len, &server_hello, &server_hello_len);
+
+    // === Send Server Hello ===
+    send_all(clientfd, server_hello, server_hello_len);
+    printf("Server Hello sent (%lu bytes)\n", server_hello_len);
+
+    // Clean up
+    close(clientfd);
+    close(sockfd);
+    free(client_hello);
+    free(cert);
+    free(ephemeral_key);
+    free(eph_pubkey);
+    free(sig_input);
+    free(signature);
+    free(nonce_tlv);
+    free(cert_tlv);
+    free(pubkey_tlv);
+    free(sig_tlv);
+    free(inner);
+    free(server_hello);
 
     return 0;
 }
